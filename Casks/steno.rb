ccde8ea7357f21e17771464361774825e9b023b0
kmg <kmganesh@kmganesh.com>
2026-04-22 10:04:26 -0700

    Bump to v0.2.15 — live transcription includes system audio

--- Casks/steno.rb
+++ Casks/steno.rb
@@ -1,6 +1,6 @@
 cask "steno" do
-  version "0.2.14"
-  sha256 "de915ac1d5ef266ca353d4ba98df77515f597871c1df784e3d2b18d4e2375318"
+  version "0.2.15"
+  sha256 "72b5639aeef8e32f681ff2e683687ddb21f4278c402c063646618a325d8370d5"
 
   url "https://github.com/kmg/steno/releases/download/v#{version}/Steno-v#{version}.dmg"
   name "Steno"
